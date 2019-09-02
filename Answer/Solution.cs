d62a85848400c9a3d004a6bd3545acdf2a51620f
neal.nankivell <neal.nankivell@gmail.com>
2019-09-02 13:09:29 +0200

    Added solution template

---
+++ Answer/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Answer
+{
+    // Reverse bits of a given 32 bits unsigned integer.
+    public class Solution
+    {
+        public uint reverseBits(uint n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
